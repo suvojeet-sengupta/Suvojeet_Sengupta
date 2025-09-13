926ceb206f61815c9443a3a7e8d1b84002077561
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-09-14 00:28:40 +0530

    fix(routing): Add basename to BrowserRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/bio_page">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
