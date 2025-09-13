e6e5fa34003b25aa5b7318f899d7b82c4e680972
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-09-14 00:31:22 +0530

    fix(layout): Correct flexbox layout for Navbar and content

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import Navbar from './Navbar';
 
 const Layout = () => {
   return (
-    <div className="bg-gray-100 dark:bg-gray-900 md:flex md:items-center md:justify-center min-h-screen">
+    <div className="bg-gray-100 dark:bg-gray-900 min-h-screen">
       <Navbar />
       <Outlet />
     </div>
