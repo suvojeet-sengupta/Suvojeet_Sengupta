23325a445bb6fd5816d8cacd97c8e8039e4387d5
suvojit21 <suvojitsengupta21@gmail.com>
2025-09-17 18:02:45 +0530

    feat: improve image placeholder in blog card

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -16,11 +16,19 @@ const BlogCard = ({ post }) => {
       variants={itemVariants}
     >
       <Link to={`/blog/${slug}`}>
-        <img
-          className="w-full h-56 object-cover object-center" 
-          src={featuredImage ? featuredImage.fields.file.url : 'https://placehold.co/600x400'}
-          alt={title}
-        />
+        {featuredImage ? (
+          <img
+            className="w-full h-56 object-cover object-center"
+            src={featuredImage.fields.file.url}
+            alt={title}
+          />
+        ) : (
+          <div className="w-full h-56 bg-dark-3 flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+            </svg>
+          </div>
+        )}
       </Link>
       <div className="p-6">
         <div className="flex items-center mb-2">
