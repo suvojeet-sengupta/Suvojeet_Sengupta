945eec11f90e443b7000e3fcbacab4040610cbfc
suvojit21 <suvojitsengupta21@gmail.com>
2025-09-20 21:01:53 +0530

    refactor: adjust footer layout for better spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,20 +15,18 @@ const Footer = () => {
               Musician, Developer, and Content Creator.
             </p>
           </div>
-          <div className="flex flex-col md:flex-row items-center">
-            <div className="mb-6 md:mb-0 md:mr-6 text-center">
-              <h3 className="text-xl font-semibold mb-4">Quick Links</h3>
-              <ul className="flex flex-wrap justify-center md:justify-start space-x-4 sm:space-x-6">
-                <li><Link to="/" className="hover:text-gray-300 transition-colors duration-300">Home</Link></li>
-                <li><Link to="/about" className="hover:text-gray-300 transition-colors duration-300">About</Link></li>
-                <li><Link to="/music" className="hover:text-gray-300 transition-colors duration-300">Music</Link></li>
-                <li><Link to="/blog" className="hover:text-gray-300 transition-colors duration-300">Blog</Link></li>
-              </ul>
-            </div>
-            <div className="text-center">
-              <h3 className="text-xl font-semibold mb-4">Follow Me</h3>
-              <SocialLinks />
-            </div>
+          <div className="mb-6 md:mb-0 text-center">
+            <h3 className="text-xl font-semibold mb-4">Quick Links</h3>
+            <ul className="flex flex-wrap justify-center md:justify-start space-x-4 sm:space-x-6">
+              <li><Link to="/" className="hover:text-gray-300 transition-colors duration-300">Home</Link></li>
+              <li><Link to="/about" className="hover:text-gray-300 transition-colors duration-300">About</Link></li>
+              <li><Link to="/music" className="hover:text-gray-300 transition-colors duration-300">Music</Link></li>
+              <li><Link to="/blog" className="hover:text-gray-300 transition-colors duration-300">Blog</Link></li>
+            </ul>
+          </div>
+          <div className="text-center">
+            <h3 className="text-xl font-semibold mb-4">Follow Me</h3>
+            <SocialLinks />
           </div>
         </div>
         <div className="mt-8 border-t border-gray-800 pt-6 text-center">
