aacb0bd3211f042521c4067742650903e1ee3704
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2025-09-22 12:48:01 +0530

    fix: Ensure consistent dark theme by removing layout background override

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,7 +11,7 @@ import BackToTopButton from './BackToTopButton';
  */
 const Layout = () => {
   return (
-    <div className="bg-gray-100 dark:bg-gray-900 min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-grow">
         <Outlet />
