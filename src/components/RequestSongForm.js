4187aeccd3046cbc541fd50896fd1eba4503e156
suvojit213 <suvojitsengupta21@gmail.com>
2025-09-29 12:14:19 +0530

    style: make song request modal background opaque

--- src/components/RequestSongForm.js
+++ src/components/RequestSongForm.js
@@ -69,7 +69,7 @@ const RequestSongForm = ({ onClose }) => {
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-100 flex items-center justify-center z-50">
       <div className="bg-dark-2 p-8 rounded-lg shadow-lg max-w-3xl w-full relative">
         <button onClick={() => {
           dispatch({type: 'RESET'});
